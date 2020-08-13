251c72cd01f6e4177dbd2fcaab1ab42f129d3c30
Mike <uc3845_mEga8>
2020-08-13 10:49:33 +0300

    hw27

---
+++ src/main/java/HW27/StreamOfStudents.java
@@ -0,0 +1,31 @@
+package HW27;
+
+import HW19.Student;
+
+import java.util.List;
+
+public class StreamOfStudents {
+    public static void main(String[] args) {
+        final int QUANTITY = 20;
+        List<Student> students = Student.studentGenerator(QUANTITY);
+        students.stream()
+                .map(Student::getLastName)
+                .map(String::toUpperCase)
+                .filter(StreamOfStudents::vowels)
+                .sorted()
+                .forEach(System.out::println);
+    }
+
+    private static boolean vowels(String s) {
+        int counter = 3;
+        String vowelStr = "AaEeIiOoUuYy";//можно сократить алфавит до больших букв, если уж используем после  .map(String::toUpperCase)
+        char[] word = s.toCharArray();
+        for (char w : word) {
+            if (vowelStr.indexOf(w) != -1) {
+                counter --;
+                if (counter == 0) return true;
+            }
+        }
+        return false;
+    }
+}
